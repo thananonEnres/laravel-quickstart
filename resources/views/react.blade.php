c032898eabb552ef6b6ab8fbd863bd4dc6203953
Thananon Kumkrong <thananon@enres.co>
2017-07-25 11:55:23 +0700

    add exercise

---
+++ resources/views/react.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.6.1/react.min.js"></script>
+  <p>hello, reactjs</p>
+
+</body>
+</html>
